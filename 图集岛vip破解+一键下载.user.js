0876659b583aa9ebce984c75a1079caaaf276132
why2fly <flyte2012@163.com>
2022-10-18 22:26:37 +0800

    pc显示2到3列

--- 图集岛vip破解+一键下载.user.js
+++ 图集岛vip破解+一键下载.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 图集岛VIP破解+下载+收藏
 // @namespace http://tampermonkey.net/
-// @version 1.0.4
+// @version 1.0.5
 // @description 图集岛最好用的插件！兼容手机 + PC ！支持VIP破解 + 单张下载 + 批量下载 + 打包下载 + 本地收藏
 // @author tengfei + ss548
 // @include /https?:\/\/(\w+\.)?tujidao.\w+/
@@ -29,7 +29,7 @@
         "a:link{color:pink;}a:visited{color:purple;}" +
         "@media screen and (min-width: 0px) {.imgbox {max-width: 100%} .contianer{column-count: 1;column-gap: 5px;margin:0;padding:0;}}" +
         "@media screen and (min-width: 900px) {.imgbox {max-width: 100%} .contianer{column-count: 2;column-gap: 5px;margin:0;padding:0;}}" +
-        "@media screen and (min-width: 1200px) {.imgbox {max-width: 100%} .contianer{column-count: 3;column-gap: 5px;margin:0;padding:0;}}" +
+        "@media screen and (min-width: 1200px) {.imgbox {max-width: 33%} .contianer{column-count: 1;column-gap: 5px;margin:0;padding:0;}}" +
         ".loading-box{width: 100%;height: 50px;display: none;align-items: center;position: absolute;top: 50px;left: 0;z-index: 999;}.loading{margin: 0 auto;width: 120px;height: 40px;line-height: 40px;color: #FFF;font-size: 17px;text-align: center;background: #1111119e;border-radius: 2px;}"+
         "</style></head>" +
         '<body bgcolor="#27282d"><div class="loading-box"><p class="loading">下载中...</p></div><ul id="images" align="center" class="contianer">';
